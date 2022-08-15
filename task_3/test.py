4b1a2d77e57bb2ee505c9946315fede88d505b13
Eleonora <norka2233@gmail.com>
2022-08-15 13:46:49 +0300

    min max average testcases

--- task_3/test.py
+++ task_3/test.py
@@ -4,11 +4,11 @@ import unittest
 from main import Note, create_csv, read_file
 
 note_charlie = Note('Charlie and the Chocolate Fabric', 'super interesting', 5)
-note_marley = Note('Marley and me', 'super sad', 4.8)
+note_marley = Note('Marley and Me', 'super sad', 4.8)
 file = 'film_notes.csv'
 
-class TestFilmNotes(unittest.TestCase):
 
+class TestFilmNotes(unittest.TestCase):
 
     def setUp(self):
         create_csv()
@@ -22,6 +22,25 @@ class TestFilmNotes(unittest.TestCase):
         film_name = 'Charlie and the Chocolate Fabric'
         self.assertEqual(note_charlie.remove_note(film_name), f"Film {film_name} was removed from list")
 
-    # def test_remove_note_incorrect_input(self):
-    #     film_name = 5
-    #     self.assertEqual(note.remove_note(film_name), TypeError)
+    def test_print_note(self):
+        film_name = "Marley and me"
+        note_marley.add_note(file)
+        # with open('film_notes.csv', 'r') as f:
+        #     reader = csv.reader(f)
+        #     [print(i) for i in reader]
+        self.assertEqual(note_marley.print_note(film_name), None)
+
+    def test_get_lowest_rate(self):
+        result = ['Harry Potter and Half-Blood Prince']
+        self.assertEqual(note_charlie.get_lowest_rate(), result)
+
+    def test_get_highest_rate(self):
+        result = ["Harry Potter and Philosopher's Stone",
+                  'Harry Potter and Goblet of Fire',
+                  'Harry Potter and Deathly Hallows Pt.2']
+        self.assertEqual(note_charlie.get_highest_rate(), result)
+
+    def test_get_average_rate(self):
+        self.assertEqual(Note.get_average_rate(self), None)
+
+
